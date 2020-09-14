ba62a0a98aeec33c0e43f2b3ba025b8fbc68d611
Derick Dsilva <mstrfpts@gmail.com>
2020-09-14 17:06:31 +0100

    added graph chart package and added redux dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,14 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./store/reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 
 const Root = () => {
-  const store = createStore(rootReducer, applyMiddleware(thunk));
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
   return (
     <Provider store={store}>
       <App />
